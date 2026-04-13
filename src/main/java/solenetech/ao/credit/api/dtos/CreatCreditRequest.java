034a89c43a6ac6684f3848b64c98c0b5661bb290
Rudilson Garcia <reinaldomagrelo5@gmail.com>
2026-04-13 12:39:12 +0100

    feat: data validation

--- src/main/java/solenetech/ao/credit/api/dtos/CreatCreditRequest.java
+++ src/main/java/solenetech/ao/credit/api/dtos/CreatCreditRequest.java
@@ -6,6 +6,21 @@ import solenetech.ao.credit.core.enuns.RequestStatus;
 
 import java.util.UUID;
 
-public record CreatCreditRequest(String id, String name, double income, int age) {
+public record CreatCreditRequest(String name, double income, int age) {
+    public CreatCreditRequest {
+        if (income <= 0 ) {
+            throw new IllegalArgumentException("Income must be positive");
+        }
+        if (age < 18 || age > 45) {
+            throw new IllegalArgumentException("Age must be between 18 and 45");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name is required");
+        }
+    }
+    public static CreatCreditRequest of(String name, Double income, Integer age)  {
+        return new CreatCreditRequest(name,income, age);
+    }
+
 }
 
--- src/test/java/solenetech/ao/credit/ScoreCalculatorWorkerMockitoTest.java
+++ src/test/java/solenetech/ao/credit/ScoreCalculatorWorkerMockitoTest.java
@@ -1,4 +1,35 @@
 package solenetech.ao.credit;
 
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import solenetech.ao.credit.workflow.worker.CalculateScoreWorker;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class ScoreCalculatorWorkerMockitoTest {
+    @Mock
+    private ActivatedJob job;
+    private final CalculateScoreWorker worker = new CalculateScoreWorker();
+    @Test
+    void testCalculateScoreWithMockito (){
+        Map<String, Object> variables = Map.of(
+                "income", 10000.0,
+                "age", 40
+        );
+        when(job.getVariablesAsMap()).thenReturn(variables);
+
+        // When
+        Map<String, Object> result = worker.calculateScore(job);
+
+        // Then
+        assertEquals(250.0, result.get("score"));
+        verify(job, times(1)).getVariablesAsMap();
+    }
 }
